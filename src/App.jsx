dae4814f5748bb2809ec1f70598bb0abe1edd2c4
Satya Fariz <satyafariznur@gmail.com>
2022-06-25 17:11:48 +0700

    add dijkstra algo

--- src/App.jsx
+++ src/App.jsx
@@ -116,6 +116,78 @@ function App() {
     setStartPos([1, 1])
     setTargetPos([_grid.length - 2, _grid[0].length - 2])
   }
+
+  const dijkstra = () => {
+    const visitedNodesInOrder = []
+    const startCell = startPos()
+    const finishCell = targetPos()
+    const _grid = grid()
+    const _wall = wall()
+
+    const unvisitedCells = []
+    const cellRef = {}
+
+    for(let i = 0; i < _grid.length; i++) {
+      for(let j = 0; j < _grid[0].length; j++) {
+        const prevCell = null
+        const visited = false
+        const position = [i, j]
+        const distance = i === startCell[0] && j === startCell[1] ? 0 : Infinity
+        const cell = {
+          position,
+          distance,
+          visited,
+          prevCell
+        }
+        unvisitedCells.push(cell)
+        cellRef[position.join('_')] = cell
+      }
+    }
+
+    while (unvisitedCells.length) {
+      unvisitedCells.sort((a, b) => a.distance - b.distance)
+      const closestCell = unvisitedCells.shift()
+      
+      // If we encounter a wall, we skip it.
+      if (_wall[closestCell.position.join('_')]) continue
+
+      // If the closest node is at a distance of infinity,
+      // we must be trapped and should therefore stop.
+      if (closestCell.distance === Infinity) return visitedNodesInOrder
+
+      closestCell.visited = true
+      visitedNodesInOrder.push(closestCell)
+
+      if (closestCell.position[0] === finishCell[0] && closestCell.position[1] === finishCell[1])
+        return visitedNodesInOrder
+
+      updateUnvisitedNeighbors(closestCell, cellRef, _grid)
+    }
+  }
+
+  const getUnvisitedNeighbors = (cell, cellRef, grid) => {
+    const neighbors = []
+    const [row, col] = cell.position
+
+    if (row > 0) neighbors.push(cellRef[`${row - 1}_${col}`])
+    if (row < grid.length - 1) neighbors.push(cellRef[`${row + 1}_${col}`])
+    if (col > 0) neighbors.push(cellRef[`${row}_${col - 1}`])
+    if (col < grid[0].length - 1) neighbors.push(cellRef[`${row}_${col + 1}`])
+    return neighbors.filter(neighbor => !neighbor.visited)
+  }
+
+  const updateUnvisitedNeighbors = (cell, cellRef, grid) => {
+    const unvisitedNeighbors = getUnvisitedNeighbors(cell, cellRef, grid)
+    for(const neighbor of unvisitedNeighbors) {
+      neighbor.distance = cell.distance + 1
+      neighbor.prevCell = cell
+    }
+  }
+
+  const visualize = () => {
+    const order = dijkstra()
+    console.log(order)
+  }
   
   return (
     <div class={styles.App}>
@@ -126,6 +198,10 @@ function App() {
 
         <button onClick={generateMaze}>
           Maze
+        </button>
+
+        <button onClick={visualize}>
+          Visualize
         </button>
       </div>
       
