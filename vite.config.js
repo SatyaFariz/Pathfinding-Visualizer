e91f29696a91301e97afc3fc9e5b5e821c71abcf
Satya Fariz <satyafariznur@gmail.com>
2022-06-26 14:40:50 +0700

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [solidPlugin()],
+    build: {
+      target: 'esnext',
+      polyfillDynamicImport: false,
+    },
+    define: {
+      __APP_ENV__: env.NODE_ENV
+    }
+  }
 });
